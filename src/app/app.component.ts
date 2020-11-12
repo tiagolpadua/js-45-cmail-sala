1b082418825e423aa5ab9b0a404a7c57e538290f
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-12 11:54:20 -0300

    d4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cmail-root',
@@ -8,11 +9,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   private _isNewEmailFormOpen = false;
 
+  emailList = [];
+
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  };
+
   get isNewEmailFormOpen(): boolean {
     return this._isNewEmailFormOpen;
   }
 
   toggleNewEmailForm(): void {
     this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
+
+  handleNewEmail(formEmail: NgForm): void {
+    if (formEmail.invalid) {
+      return;
+    }
+
+    this.emailList.push(this.email);
+
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    };
+
+    formEmail.reset();
   }
 }
